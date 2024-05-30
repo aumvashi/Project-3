191acd87edeca2c18f8bf3f3bc2423cc83cde28c
aumvashi <165994662+aumvashi@users.noreply.github.com>
2024-05-30 10:56:12 +0000

    Books Collection

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Project-3
+
 Book Management System
 
-# 106232052310523-4
+## Book Record Managment
 
-## Book Record Managment 
 Server -> Storing Book Data, User Register, Subsctibers
 
 /users/{id}
@@ -12,10 +12,12 @@ http://localhost:801/users/1
 # Routes and Endpoints
 
 ## /users
+
 POST: Create a new user
 GET: Get all users Lists
 
 ## /users/{id}
+
 GET: Get a user by id
 PUT: Update a user by id
 DELETE: Delete a user by id (chk if he/she still has an issued book) && (if he/she have any fine to be paid)
@@ -23,58 +25,55 @@ DELETE: Delete a user by id (chk if he/she still has an issued book) && (if he/s
 // Fine Desciption
 User => 3 months || 6 months || 12 months
 
-04th aug 2023  4*50 = 200
-
+04th aug 2023 4\*50 = 200
 
 ## users/subscription-details/{id}
+
 GET: Get user subscription details
+
 1. Date of subscription
 2. valid till
-3.Fine if any 
-
+   3.Fine if any
 
 ## /books
+
 GET: Get all the books
 POST: Create/Add a book
 
-
 ## /books/{id}
+
 GET: Get a book by its id
 PUT: Update a book by its id
 
-
 ## books/issued
+
 GET: Get all the issued books
 
-
 ## /books/issued/withFine
+
 GET: Get all issued books with fine
 
-
 ### Subscription Type
-Basic       (3 months)
-Standard    (6 months)
-Premium     (12 months)
 
-
-
+Basic (3 months)
+Standard (6 months)
+Premium (12 months)
 
 ## Spread Operator
+
 data ={
-    name: "rohan",
-    age: 2
+name: "rohan",
+age: 2
 }
 
 {
-    ...data,
-    age: 5,
-    surname: "kinnal"
+...data,
+age: 5,
+surname: "kinnal"
 }
 
 data={
-    name:"rohan",
-    age: 5,
-    surname: "kinnal"
-    }
-
-
+name:"rohan",
+age: 5,
+surname: "kinnal"
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
-const { users } = require("./DATA/users.json");
+const usersRouter = require("./routes/users.js");
+const booksRouter = require("./routes/books.js");
 
 const app = express();
 
@@ -14,139 +15,12 @@ app.get("/", (req, res) => {
   });
 });
 
-/*
-    Route: /users
-    Method: GET
-    Desc: Get all users
-    Access: Public
-    Parameters: None
-*/
-
-app.get("/users", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: users,
-  });
-});
-
-/*
-    Route: /users/:id
-    Method: GET
-    Desc: Get single users
-    Access: Public
-    Parameters: id
-*/
-
-app.get("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((each) => each.id === id);
-  if (!user) {
-    res.status(404).json({
-      success: false,
-      message: "User Not Found :(",
-    });
-  }
-  res.status(200).json({
-    success: true,
-    message: "User Found :)",
-    data: user,
-  });
-});
-
-/*
-    Route: /users
-    Method: POST
-    Desc: Add New User
-    Access: Public
-    Parameters: None
-*/
-
-app.post("/users", (req, res) => {
-  const { id, name, surname, email, subscriptionType, subscriptionDate } =
-    req.body;
-  const user = users.find((each) => each.id === id);
-
-  if (user) {
-    res.status(404).json({
-      success: false,
-      message: "ID Already Exixts...",
-    });
-  }
-  users.push({
-    id,
-    surname,
-    email,
-    subscriptionType,
-    subscriptionDate,
-  });
-
-  return res.status(201).json({
-    success: true,
-    message: "User Added Successfully...",
-    data: users,
-  });
-});
-
-/*
-    Route: /users/:id
-    Method: PUT
-    Desc: Updating User by ID
-    Access: Public
-    Parameters: ID
-*/
-
-app.put("/users/:id", (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-
-  const user = users.find((each) => each.id === id);
-  if (!user) {
-    return res.status(404).json({
-      success: false,
-      message: "User Doesn't Exists...",
-    });
-  }
-  const updateUserData = users.map((each) => {
-    if (each.id === id) {
-      return {
-        ...each,
-        ...data,
-      };
-    }
-    return each;
-  });
-  return res.status(200).json({
-    success: true,
-    message: "User Updated...",
-    data: updateUserData,
-  });
-});
-
-/*
-    Route: /users/:id
-    Method: DELETE
-    Desc: Deleting User by ID
-    Access: Public
-    Parameters: ID
-*/
-
-app.delete("/users/:id",(req, res) => {
-    const {id} = req.params;
-    const user = users.find((each) => each.id === id);
-    if(!user){
-        res.status(404).json({
-            success: false,
-            message: "User Doesn't Exist...",
-        });
-    }
-
-    // Logic for delete still left
-
-})
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 app.get("*", (req, res) => {
   res.status(404).json({
-    message: "This root doesnt exists :(",
+    message: "This route doesnt exists :(",
   });
 });
 
